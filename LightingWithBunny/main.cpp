827cbee9c76b01ab05ede83c1df881ae1db5af26
bearics <bearics@gmail.com>
2018-11-22 16:54:01 +0900

    Draw Bunny with triangles

--- LightingWithBunny/main.cpp
+++ LightingWithBunny/main.cpp
@@ -3,11 +3,20 @@
 #include <gl/GLU.h>
 #include <gl/glut.h>
 #include <math.h>
+#include <fstream>
 #include <iostream>
+#include <string>
+
+using namespace std;
 
 #define PI 3.14159265
 
 static float viewer[3];
+static double nPoints;
+static int nPolygons;
+static double** points;
+static int** polygons;
+
 
 /**
 	Draw Coodrination system.
@@ -32,6 +41,68 @@ void DrawCoordinationSystem(float length)
 	glEnd();
 }
 
+/**
+	Read Bunny
+*/
+void ReadBunny(string path)
+{
+	ifstream fin(path.data());
+
+	if (fin.is_open())
+	{
+		fin >> nPoints >> nPolygons;
+
+		points = new double*[nPoints];
+
+		for (int n = 0; n < nPoints; n++)
+		{	// read coordinates of points 
+			points[n] = new double[3];
+
+			fin >> points[n][0] >> points[n][1] >> points[n][2];
+		}
+
+		polygons = new int*[nPolygons];
+
+		for (int n = 0; n < nPolygons; n++)
+		{	// read sets of points in a polygons
+			polygons[n] = new int[3];
+
+			fin >> polygons[n][0] >> polygons[n][1] >> polygons[n][2];
+		}
+
+		fin.close();
+	}
+	else
+	{
+		cout << "Cannot read file" << endl;
+	}
+
+	return;
+}
+
+void DrawTorusBunny()
+{
+
+	glColor3f(0, 0, 1);
+
+	for (int polygon = 0; polygon < nPolygons; polygon++)
+	{	// Draw Bunny as Triangles with line frame
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		glBegin(GL_TRIANGLES);
+		{
+			for (int point = 0; point < 3; point++)
+			{
+				glVertex3f(
+					points[polygons[polygon][point] - 1][0],
+					points[polygons[polygon][point] - 1][1],
+					points[polygons[polygon][point] - 1][2]);
+			}
+		}
+		glEnd();
+	}
+}
+
+
 void RenderScene()
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the window
@@ -46,18 +117,23 @@ void RenderScene()
 
 
 	// Draw object
+	DrawTorusBunny();
 
 
-
+	// Flush 
 	glutSwapBuffers();
 	glFlush();
 }
 
 void init(void)
 {
-	viewer[0] = 15;
-	viewer[1] = 15;
-	viewer[2] = 15;
+	viewer[0] = 2;
+	viewer[1] = 2;
+	viewer[2] = 2;
+
+	// read Bunny
+	ReadBunny("bunny_origin.txt");
+
 }
 
 void SetupRC(void)
@@ -81,11 +157,6 @@ void ChangeSize(int w, int h)
 
 }
 
-void ResetDisplay()
-{
-
-}
-
 void Keyboard(unsigned char key, int x, int y)
 {
 
@@ -99,7 +170,7 @@ void main(int argc, char* argv[])
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
 	glutInitWindowSize(600, 600);
 	glutCreateWindow("Torus");
-	
+
 	glutReshapeFunc(ChangeSize);
 	glutDisplayFunc(RenderScene);
 	glutKeyboardFunc(Keyboard);
